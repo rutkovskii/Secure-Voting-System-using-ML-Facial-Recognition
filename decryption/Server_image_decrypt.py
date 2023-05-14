5e786fd268eb2eb9e36ff17c13fd32468f6ae7bf
brayberg <bjbergeron@umass.edu>
2023-05-13 20:28:15 -0400

    cleaned up path names

--- decryption/Server_image_decrypt.py
+++ decryption/Server_image_decrypt.py
@@ -6,34 +6,53 @@ from PIL import Image
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 import hashlib
+import shutil
 
 key_pair = RSA.generate(2048)
 public_server_key = key_pair.publickey().export_key()
 private_server_key = key_pair
-with open('C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\public_server_key.pem','wb') as f:
+
+encrypt_folder = "/Users/braydenbergeron/PycharmProjects/547_Projects/encryption/"
+decrypt_folder = "/Users/braydenbergeron/PycharmProjects/547_Projects/decryption/"
+
+public_server_key_path = '/Users/braydenbergeron/PycharmProjects/547_Projects/decryption/public_server_key.pem'
+public_client_key_path = '/Users/braydenbergeron/PycharmProjects/547_Projects/decryption/public_client_key.pem'
+
+with open(public_server_key_path,'wb') as f:
 	f.write(public_server_key)
-while not os.path.exists('C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\public_client_key.pem'):
-    time.sleep(1)
-with open('C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\public_client_key.pem', 'rb') as f:
+shutil.move(public_server_key_path,encrypt_folder)
+
+while not os.path.exists(public_client_key_path):
+	time.sleep(1)
+with open(public_client_key_path, 'rb') as f:
 	public_client_key = RSA.importKey(f.read())
 
-while not os.path.exists('C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\digital_signature.bin'):
-    time.sleep(1)
-with open('C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\digital_signature.bin', 'rb') as f:
+digital_signature_path = '/Users/braydenbergeron/PycharmProjects/547_Projects/decryption/digital_signature.bin'
+while not os.path.exists(digital_signature_path):
+	time.sleep(1)
+with open(digital_signature_path, 'rb') as f:
 	digital_signature = int.from_bytes(f.read(),byteorder='big')
 signature_hash =  pow(digital_signature,public_client_key.e,public_client_key.n)
+
+
 mode = AES.MODE_CBC
 keyLen = 32
 ivLen = AES.block_size
-with open("C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\Encrypted_AES_Key.bin", 'rb') as f:
+Encrypted_AES_Key_Path = '/Users/braydenbergeron/PycharmProjects/547_Projects/decryption/Encrypted_AES_Key.bin'
+iv_path = '/Users/braydenbergeron/PycharmProjects/547_Projects/decryption/iv.bin'
+image_path = '/Users/braydenbergeron/PycharmProjects/547_Projects/decryption/voter_image.enc'
+
+with open(Encrypted_AES_Key_Path, 'rb') as f:
 	AESKey=f.read()
 cipherAES = PKCS1_OAEP.new(private_server_key)
 key = cipherAES.decrypt(AESKey)
-with open("C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\iv.bin", 'rb') as f:
+
+with open(iv_path, 'rb') as f:
 	iv=f.read()
-image_path = "C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\Encrypted_AES_Images\\Brayden1.enc"
+
 with open(image_path,'rb') as f:
 	Encrypted_Face = f.read()
+
 # decrypt
 cipher = AES.new(key, AES.MODE_CBC, iv)
 plaintext = cipher.decrypt(Encrypted_Face)
@@ -41,8 +60,7 @@ Face_decrypted = unpad(plaintext, AES.block_size)
 
 # integrity check
 
-output_folder = "C:\\Users\\Brayd\\Documents\\UMass\\ECE_547\\ML_Encryption\\Decrypted_AES_Images\\"
-output_path = os.path.join(output_folder,"Brayden1.jpg")
+output_path = os.path.join(decrypt_folder,"voter_image.jpg")
 with open(output_path, 'wb') as f:
 	f.write(Face_decrypted)
 
@@ -53,4 +71,11 @@ if signature_hash==hashReceived:
 else:
 	print("Incorrect Hash")
 
+if (input("Remove[Y/N]")=="Y"):
+	os.remove(iv_path) # delete the AES IV
+	os.remove(Encrypted_AES_Key_Path) # delete the encrypted AES key ks+(kAES)
+	os.remove(public_client_key_path) # delete the public client key Kc+
+	os.remove(image_path) # delete the encrypted image
+	os.remove(digital_signature_path) # delete the digital signature
+	os.remove(output_path) # delete the decrypted image
 
